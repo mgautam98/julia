d9dbec76df440183c938d1bdea50b44a18d4ea92
Gautam Mishra <mishragautam96@gmail.com>
2020-06-25 00:52:06 +0530

    Add tests for rem2pi for NaN values

--- test/numbers.jl
+++ test/numbers.jl
@@ -2509,6 +2509,13 @@ end
     @test rem2pi(T(-4), RoundUp)      == -4
 end
 
+@testset "PR #36420 $T" for T in (Float16, Float32, Float64)
+    @test rem2pi(T(NaN), RoundToZero)  === T(NaN)
+    @test rem2pi(T(NaN), RoundNearest) === T(NaN)
+    @test rem2pi(T(NaN), RoundDown)    === T(NaN)
+    @test rem2pi(T(NaN), RoundUp)      === T(NaN)
+end
+
 import Base.^
 struct PR20530; end
 struct PR20889; x; end
