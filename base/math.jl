698944e13ba069eff0a11e59960ee0c0ae1b1022
Gautam Mishra <mishragautam96@gmail.com>
2020-06-25 00:17:16 +0530

    Fix rem2pi for NaN

--- base/math.jl
+++ base/math.jl
@@ -975,6 +975,8 @@ julia> rem2pi(7pi/4, RoundDown)
 """
 function rem2pi end
 function rem2pi(x::Float64, ::RoundingMode{:Nearest})
+    isnan(x) && return NaN
+
     abs(x) < pi && return x
 
     n,y = rem_pio2_kernel(x)
@@ -998,6 +1000,8 @@ function rem2pi(x::Float64, ::RoundingMode{:Nearest})
     end
 end
 function rem2pi(x::Float64, ::RoundingMode{:ToZero})
+    isnan(x) && return NaN
+
     ax = abs(x)
     ax <= 2*Float64(pi,RoundDown) && return x
 
@@ -1023,6 +1027,8 @@ function rem2pi(x::Float64, ::RoundingMode{:ToZero})
     copysign(z,x)
 end
 function rem2pi(x::Float64, ::RoundingMode{:Down})
+    isnan(x) && return NaN
+
     if x < pi4o2_h
         if x >= 0
             return x
@@ -1052,6 +1058,8 @@ function rem2pi(x::Float64, ::RoundingMode{:Down})
     end
 end
 function rem2pi(x::Float64, ::RoundingMode{:Up})
+    isnan(x) && return NaN
+    
     if x > -pi4o2_h
         if x <= 0
             return x
